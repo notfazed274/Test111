2f7900bafce061744244dad0900387dffad22370
kindalab-fede <fpalacio@kindalab.co>
2021-12-01 10:48:39 -0300

    Metodo y assertion

--- Pages/Kindalab_homepage.py
+++ Pages/Kindalab_homepage.py
@@ -1,10 +1,16 @@
+from selenium.webdriver.common.by import By
 
 class Kinda:
 
     base_url = "https://kindalab.co/"
+    titulo = (By.CLASS_NAME, 'title')
 
     def __init__(self, driver):
         self.driver = driver
 
     def abrir_url(self):
         self.driver.get(Kinda.base_url)
+
+    def ver_titulo(self):
+        titulo = self.driver.find_element(*self.titulo)
+        return titulo.text
--- tests/test_suite.py
+++ tests/test_suite.py
@@ -6,4 +6,6 @@ class Test_config:
     def test_abrir_url(self, browser):
         k = Kinda(browser)
         k.abrir_url()
+        assert k.ver_titulo() == 'WE ARE\n DEVELOPERS'
+        print(k.ver_titulo())
 
